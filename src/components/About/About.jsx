5a7af76cb9d61e0f49e1cf46e16714c57aa81f89
JoaoMoraisxD <joaomoraisxd@gmail.com>
2026-07-21 09:54:10 -0300

    Regenera imagem

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -32,7 +32,7 @@ export default function About() {
                 >
 
                     <img
-                        src="/images/about.png"
+                        src={`${import.meta.env.BASE_URL}images/about.png`}
                         alt="Didô"
                     />
 
